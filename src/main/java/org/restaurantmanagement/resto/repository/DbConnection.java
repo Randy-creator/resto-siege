a7f38bdadbee636390479bc969937ac3133d1fe8
Randy-creator <hei.randy.2@gmail.com>
2025-04-08 22:03:38 +0300

    feat: add Dbconnection.class implementation to initialize a connection through the PSQL database

--- src/main/java/org/restaurantmanagement/resto/repository/DbConnection.java
+++ src/main/java/org/restaurantmanagement/resto/repository/DbConnection.java
@@ -1,4 +1,21 @@
 package org.restaurantmanagement.resto.repository;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DbConnection {
+    Connection connection = null;
+
+    public Connection getConnection() {
+        try {
+            connection = DriverManager.getConnection(System.getenv("url"), System.getenv("user"), System.getenv("password"));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
+
+//   "jdbc:postgresql://localhost:5432/restaurant"
+
 }
