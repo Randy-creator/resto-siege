321258eb2c0117bcb35da604e15f78fe88f183d2
Randy-creator <hei.randy.2@gmail.com>
2025-04-10 18:35:17 +0300

    feat: add method getIngredientOfDish method to get all ingredient used to make a dish

--- src/main/java/org/restaurantmanagement/resto/repository/dao/DishCrudImpl.java
+++ src/main/java/org/restaurantmanagement/resto/repository/dao/DishCrudImpl.java
@@ -1,6 +1,8 @@
 package org.restaurantmanagement.resto.repository.dao;
 
 import org.restaurantmanagement.resto.entity.Dish;
+import org.restaurantmanagement.resto.entity.Ingredient;
+import org.restaurantmanagement.resto.entity.Unit;
 import org.restaurantmanagement.resto.repository.DbConnection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -16,8 +18,37 @@ public class DishCrudImpl implements DishCrud {
 
     private final DbConnection db;
 
+    @Autowired
     public DishCrudImpl(DbConnection db) {
         this.db = db;
+    }
+
+
+    private List<Ingredient> getIngredientOfDish(Long dishId) {
+        String sql = """
+                SELECT i.ingredient_id, i.ingredient_name, i.quantity, i.unit
+                FROM DishIngredient di JOIN Ingredient i ON di.ingredient_id=i.ingredient_id
+                JOIN Dish d ON di.dish_id=d.dish_id WHERE d.dish_id = ?
+                """;
+
+        List<Ingredient> ingredientList = new ArrayList<>();
+        try (Connection connection = db.getConnection(); PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setLong(1, dishId);
+
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    ingredientList.add(new Ingredient(
+                            rs.getLong("ingredient_id"),
+                            rs.getString("ingredient_name"),
+                            rs.getDouble("quantity"),
+                            Unit.valueOf(rs.getString(("unit")))
+                    ));
+                }
+            }
+            return ingredientList;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
@@ -36,7 +67,8 @@ public class DishCrudImpl implements DishCrud {
                     dishList.add(new Dish(
                             rs.getLong("dish_id"),
                             rs.getString("dish_name"),
-                            rs.getDouble("price")
+                            rs.getDouble("dish_price"),
+                            getIngredientOfDish(rs.getLong("dish_id"))
                     ));
                 }
             }
