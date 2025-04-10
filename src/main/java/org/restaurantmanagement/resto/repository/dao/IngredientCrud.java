69e5ef663480491170018adcce5095f4bdb39396
Randy-creator <hei.randy.2@gmail.com>
2025-04-10 20:50:09 +0300

    feat: add forgotten method on the interface

--- src/main/java/org/restaurantmanagement/resto/repository/dao/IngredientCrud.java
+++ src/main/java/org/restaurantmanagement/resto/repository/dao/IngredientCrud.java
@@ -1,4 +1,9 @@
 package org.restaurantmanagement.resto.repository.dao;
 
+import org.restaurantmanagement.resto.entity.Ingredient;
+
+import java.util.List;
+
 public interface IngredientCrud {
+    public List<Ingredient> getAllIngredients(int page, int size);
 }
