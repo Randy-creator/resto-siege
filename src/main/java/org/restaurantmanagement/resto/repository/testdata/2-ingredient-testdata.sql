1d18ba5c573a59fd9c0da04af0ceb60772e21b57
Randy-creator <hei.randy.2@gmail.com>
2025-04-14 21:07:07 +0300

    feat: add mock data for: dish, ingredients, price and stockMovement

---
+++ src/main/java/org/restaurantmanagement/resto/repository/testdata/2-ingredient-testdata.sql
@@ -0,0 +1,5 @@
+INSERT INTO Ingredient (ingredient_id, ingredient_name, unit) VALUES
+(1, 'Bread', 'U'),
+(2, 'Sausage','G'),
+(3, 'Oil','L'),
+(4, 'Egg','U');
---
+++ src/main/java/org/restaurantmanagement/resto/repository/testdata/3-dish-ingredient-testdata.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO DishIngredient (dish_id, ingredient_id, quantity) VALUES
+(1, 1, 1),
+(1, 2, 100),
+(1, 3, 0.15),
+(1, 4, 1);
