8fdb1e91c92842c3d54723fc8ba7529e926e2ed6
radiant_wizard <hei.taratra@gmail.com>
2025-04-18 16:54:30 +0300

    chore(controller): added the endpoint to get  the Processing time for a dish

--- src/main/java/org/restaurantmanagement/resto/controller/DashBoardController.java
+++ src/main/java/org/restaurantmanagement/resto/controller/DashBoardController.java
@@ -1,17 +1,19 @@
 package org.restaurantmanagement.resto.controller;
 
+import org.restaurantmanagement.resto.entity.Enum.Mode;
+import org.restaurantmanagement.resto.service.dish.DishServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
+@RequestMapping("/branch")
 public class DashBoardController {
+    @Autowired
+    private DishServiceImpl dishService;
 
     @GetMapping("/bestSales ")
     public ResponseEntity<Object> getBestSales(@RequestParam LocalDateTime start,
@@ -22,13 +24,24 @@ public class DashBoardController {
         throw new RuntimeException("not yet");
     }
 
-    @GetMapping("/dishes/{id}/processingTime")
+    @GetMapping("/{branchId}/dishes/{id}/processingTime")
     public ResponseEntity<Object> getProcessingTime(
-            @PathVariable Long id,
-            @RequestParam(defaultValue = "SECOND", required = false) String durationType,
-            @RequestParam(defaultValue = "AVERAGE", required = false) String processingTimeType) {
-
-        throw new RuntimeException("not yet");
-
+            @PathVariable Long dishId,
+            @RequestParam(defaultValue = "SECONDS", required = false) TimeUnit durationType,
+            @RequestParam(defaultValue = "AVG", required = false) Mode mode,
+            @RequestParam LocalDateTime start,
+            @RequestParam LocalDateTime end) {
+        try {
+            double processingTime = dishService.calculateProcessingTimeForDishOrders(
+                    dishId,
+                    start,
+                    end,
+                    durationType,
+                    mode
+            );
+            return ResponseEntity.ok(dishId + "processing time : " + processingTime);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
