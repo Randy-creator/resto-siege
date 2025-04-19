290c0ab6d001641344c9690b2bb21e3e49690bb3
Randy-creator <hei.randy.2@gmail.com>
2025-04-19 12:55:52 +0300

    feat: add endpoint for getBestSales

--- src/main/java/org/restaurantmanagement/resto/service/Sale/SaleServiceImpl.java
+++ src/main/java/org/restaurantmanagement/resto/service/Sale/SaleServiceImpl.java
@@ -67,4 +67,9 @@ public class SaleServiceImpl implements SaleService {
             }
         }
     }
+
+    @Override
+    public List<Sale> getBestSale(int top) {
+        return saleCrud.getBestSale(top);
+    }
 }
