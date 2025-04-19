a02523edd3abea2ad856b23d76eb4f8ed331036e
radiant_wizard <hei.taratra@gmail.com>
2025-04-19 11:23:45 +0300

    fix(Sale): added no args default constructor for the Jackson Parsing

--- src/main/java/org/restaurantmanagement/resto/entity/model/Sale.java
+++ src/main/java/org/restaurantmanagement/resto/entity/model/Sale.java
@@ -3,8 +3,10 @@ package org.restaurantmanagement.resto.entity.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class Sale {
     private String dishName;
