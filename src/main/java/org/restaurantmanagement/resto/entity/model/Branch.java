4f46801a9b26370ad8ee052461e1cd823262a56a
radiant_wizard <hei.taratra@gmail.com>
2025-04-19 12:54:56 +0300

    chore(branch): camelCased the attributes name

--- src/main/java/org/restaurantmanagement/resto/entity/model/Branch.java
+++ src/main/java/org/restaurantmanagement/resto/entity/model/Branch.java
@@ -8,9 +8,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Data
 public class Branch {
-    private Long branch_id;
-    private String branch_name;
-    private String branch_url;
-    private String branch_api_key;
+    private Long branchId;
+    private String branchName;
+    private String branchUrl;
+    private String branchApiKey;
 }
 
