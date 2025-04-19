515156e6b0495e1869b3d97d467766951045b593
radiant_wizard <hei.taratra@gmail.com>
2025-04-19 11:55:55 +0300

    fix(branch_migration): fix misspelling

--- src/main/resources/db/migration/2_branch_migration.sql
+++ src/main/resources/db/migration/2_branch_migration.sql
@@ -2,5 +2,5 @@ CREATE TABLE branches(
     branch_id BIGINT primary key,
     branch_name VARCHAR(255) UNIQUE,
     branch_url VARCHAR(255) UNIQUE,
-    branch_api_key VARCHAR(255) UNIQUE,
+    branch_api_key VARCHAR(255) UNIQUE
 );
